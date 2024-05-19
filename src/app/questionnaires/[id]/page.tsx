c9372735da2aa8589925bee5f506d794e9e39cca
SJanna <sjannadiaz@gmail.com>
2024-05-18 22:54:37 -0500

    Refactor Questionnaire(Response) to use generic functions

--- src/app/questionnaires/[id]/page.tsx
+++ src/app/questionnaires/[id]/page.tsx
@@ -1,23 +1,34 @@
-import { FhirError } from "@/errors/FhirError"
-import QuestionnaireResponseForm from "@/features/questionnaires/components/QuestionnaireResponseForm"
-import getQuestionnaire from "@/features/questionnaires/server/getQuestionnaire"
+import { FhirError } from "@/errors/FhirError";
+import QuestionnaireResponseForm from "@/features/questionnaires/components/QuestionnaireResponseForm";
+import { getResource } from "@/server/getResource";
+import { questionnaireSchema } from "@/types/Questionnaire";
 
+export default async function QuestionnairesIdPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  try {
+    const questionnaire = await getResource({
+      id: params.id,
+      schema: questionnaireSchema,
+    });
 
-export default async function QuestionnairesIdPage({ params }: { params: { id: string } }) {
-    try {
-        const questionnaire = await getQuestionnaire(params.id)
-
-        return (
-            <>
-                {/* ...Building form */}
-                <QuestionnaireResponseForm questionnaire={questionnaire} />
-            </>
-        )
-    } catch (error) {
-        if (error instanceof FhirError) {
-            // show here Fhir Error
-            return <div>{error.errorSchema?.text?.div ?? ''}</div>
-        }
-        return <div>Error fetching questionnaire.</div>
+    if (questionnaire === null) {
+      return <div>Questionnaire not found.</div>;
     }
+
+    return (
+      <>
+        {/* ...Building form */}
+        <QuestionnaireResponseForm questionnaire={questionnaire} />
+      </>
+    );
+  } catch (error) {
+    if (error instanceof FhirError) {
+      // show here Fhir Error
+      return <div>{error.errorSchema?.text?.div ?? ""}</div>;
+    }
+    return <div>Error fetching questionnaire.</div>;
   }
+}
